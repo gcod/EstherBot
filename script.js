9a879d3a6c074a6dd29d55d8c8ebbc77d5a17bb9
gcod <gcoder@gmail.com>
2016-04-21 21:55:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Bonjour! Dans quelle industrie se trouve le marchand que vous cherchez? (Ex: AUTOMOBILE, MAISON ET JARDINAGE, RESTAURANT, SANTÉ, HOTEL) ')
                 .then(() => 'speak');
         }
     },
